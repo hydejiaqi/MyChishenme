79e6d1411958db83231935e5b8a0a36ba597229a
jjiang@autelrobotics.com <jjiang@autelrobotics>
2018-06-01 17:40:01 -0700

    add restaurant table

--- app/src/main/java/com/chishenme/jjiang/chishenme/db/config/RestaurantListConfig.java
+++ app/src/main/java/com/chishenme/jjiang/chishenme/db/config/RestaurantListConfig.java
@@ -4,13 +4,14 @@ import com.chishenme.jjiang.chishenme.db.DBTable;
 import com.chishenme.jjiang.chishenme.db.DatabaseType;
 import com.chishenme.jjiang.chishenme.db.DbHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by jjiang on 6/1/2018.
  */
 
-public class FoodListConfig extends DbConfig{
+public class RestaurantListConfig extends DbConfig{
     private DbHelper dbHelper;
 
     public static final String TABLE_NAME = "restaurant_list_table";
@@ -18,13 +19,24 @@ public class FoodListConfig extends DbConfig{
     public static final String LIST_NAME = "listname";
     public static final String RESTAURANT_ID = "res_id";
 
-    FoodListConfig(DatabaseType databaseType) {
+    RestaurantListConfig(DatabaseType databaseType) {
         super(DatabaseType.RESTAURANT);
     }
 
     @Override
     protected List<DBTable> initTables() {
-        return null;
+        List<DBTable> tables = new ArrayList<>();
+        try {
+            DBTable list = new DBTable(TABLE_NAME);
+            list.addIntegerKey(ID, true);
+            list.addTextKey(LIST_NAME);
+            list.addIntegerKey(RESTAURANT_ID);
+            list.join2List(tables);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return tables;
     }
 
     @Override
