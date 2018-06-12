249af7ff70c9a460fe7fa8eef3d9b123ca6e16af
jjiang@autelrobotics.com <jjiang@autelrobotics>
2018-06-11 17:41:55 -0700

    add restaurant pool

--- app/src/main/java/com/chishenme/jjiang/chishenme/adapter/PoolAdapter.kt
+++ app/src/main/java/com/chishenme/jjiang/chishenme/adapter/PoolAdapter.kt
@@ -3,12 +3,16 @@ package com.chishenme.jjiang.chishenme.adapter
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.RecyclerView.Adapter
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 
 /**
  * Created by jjiang on 5/24/2018.
  */
-class PoolAdapter(val context: Context) : Adapter<RecyclerView.ViewHolder>() {
+class PoolAdapter(val context: Context) : RecyclerView.Adapter<PoolAdapter.ViewHolder>() {
+    class ViewHolder(val textView: TextView, val image: ImageView) : RecyclerView.ViewHolder(textView)
 
     var poollist: ArrayList<String>
     get() { return poollist}
@@ -21,16 +25,22 @@ class PoolAdapter(val context: Context) : Adapter<RecyclerView.ViewHolder>() {
         this.poollist.add(item)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PoolAdapter.ViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+      //  val view = LayoutInflater.from(parent.context).inflate(R.layout.adapter_pool, parent, false)
+
+
     }
 
     override fun getItemCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return poollist.size;
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.textView.text = poollist.get(position)
+
     }
 
 
